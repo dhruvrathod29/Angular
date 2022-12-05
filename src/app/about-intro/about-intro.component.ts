e4159554feb82e0638b26481c221224df67fd1ae
Dhruv Rathod <86467492+dhruvrathod29@users.noreply.github.com>
2022-12-05 11:41:27 +0530

    'about','home','UI','footer','header'

---
+++ src/app/about-intro/about-intro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-intro',
+  templateUrl: './about-intro.component.html',
+  styleUrls: ['./about-intro.component.css']
+})
+export class AboutIntroComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IntroSectionComponent } from './intro-section/intro-section.component';
+import { AboutIntroComponent } from './about-intro/about-intro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent,
+    IntroSectionComponent,
+    AboutIntroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
